77856377eec2d8cfa7eff6643ab5287625fd0109
khadije <khadijeh.allahgholi@gmail.com>
2022-05-05 19:42:10 +0430

    q2 changed

--- app.js
+++ app.js
@@ -15,61 +15,60 @@
 
 
 //Ex.2
-// function starRatings(array){
-//     let sum = 0;
-//     let res;
-//     let round;
-//     let getSum = array.map((item,index) => item * (index + 1), 0)
-//         .reduce((acc,val) => acc + val ,0);
-//     for (let i of array){
-//         sum += i;
-//     }
-//     res = (getSum/sum).toFixed(2);
-//     round = Math.round(res);
+function starRatings(array){
+    let sum = 0;
+    let res;
+    let round;
+    let getSum = array.map((item,index) => item * (index + 1), 0)
+        .reduce((acc,val) => acc + val ,0);
+    for (let i of array){
+        sum += i;
+    }
+    res = (getSum/sum).toFixed(2);
+    round = Math.round(res);
     
-//     for (let i=1 ; i<=round ; i++){
-//         console.log("*");
-//     }
-//         return res;       
-// }
-// let array = [0,2,0,1,23];
-// console.log(starRatings(array));
+    for (let i=1 ; i<=round ; i++){
+        console.log(`"[${res}]  *"`);
+    } 
+}
+let array = [0,2,0,1,23];
+starRatings(array);
 
 //EX.3
-let user = {
-    id: 6,
-    name: "Hossein",
-    lastName: "Zenderoh",
-    roles: ["student", "admin"],
-    age: 19,
-    scores: { english: 10, math: 19.5, physics: 14 }
-};
-//Ex.3-1
-function newCourseScore(key , newObj){
-    user[key] = {...user[key],...newObj};
-    console.log(user);
-}
-let newObj = {chemistry: 15};
-newCourseScore('scores' , newObj);
+// let user = {
+//     id: 6,
+//     name: "Hossein",
+//     lastName: "Zenderoh",
+//     roles: ["student", "admin"],
+//     age: 19,
+//     scores: { english: 10, math: 19.5, physics: 14 }
+// };
+// //Ex.3-1
+// function newCourseScore(key , newObj){
+//     user[key] = {...user[key],...newObj};
+//     console.log(user);
+// }
+// let newObj = {chemistry: 15};
+// newCourseScore('scores' , newObj);
 
-//Ex.3-2
-function newRole(key , role , age){
-    user[key] = {...user[key],role};
-    console.log(user);
-    if (age<18){
-        user = {...user,veryyoung: [role]};
-        console.log(user);
-    }else{
-        console.log("age is more than of 18");
-    }
-}
-newRole('roles' , 'student2', 15);
+// //Ex.3-2
+// function newRole(key , role , age){
+//     user[key] = {...user[key],role};
+//     console.log(user);
+//     if (age<18){
+//         user = {...user,veryyoung: [role]};
+//         console.log(user);
+//     }else{
+//         console.log("age is more than of 18");
+//     }
+// }
+// newRole('roles' , 'student2', 15);
 
-//Ex.3-2
-let array = Object.entries(user);
-console.log(array);
-user = {...user,fatherName: "ali"}
-console.log(user);
+// //Ex.3-2
+// let array = Object.entries(user);
+// console.log(array);
+// user = {...user,fatherName: "ali"}
+// console.log(user);
 
 //Ex.4
 // function time(date){
